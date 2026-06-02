1eb8280f8719c99bcbb1831057688d66c2e382ed
kibet23-ally <kibet2653@gmail.com>
2026-06-02 17:02:43 +0300

    Update accept-invite.tsx

--- src/routes/accept-invite.tsx
+++ src/routes/accept-invite.tsx
@@ -1,7 +1,9 @@
+// src/routes/accept-invite.tsx
 import { createFileRoute } from '@tanstack/react-router'
 import AcceptInvite from '@/pages/AcceptInvite'
 import { z } from 'zod'
 
+// Validates the ?token= query param from the URL
 const searchSchema = z.object({
   token: z.string().optional(),
 })
